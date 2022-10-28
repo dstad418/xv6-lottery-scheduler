e88dd0b497f75a267adc3d9505470b50494bf797
Dylan H <dstad41@hotmail.com>
2022-10-28 15:36:35 -0700

    Add files via upload
    Push #2 with updated lapic, defs.h and date.h pragma/ifndef

---
+++ date.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _DATE_H
+#define _DATE_H
+
+
+struct rtcdate {
+  uint second;
+  uint minute;
+  uint hour;
+  uint day;
+  uint month;
+  uint year;
+};
+
+
+#endif // _DATE_H
